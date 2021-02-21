f31fef062062606da3e4da1d4f6d10bf34db3d79
Hasan Aykaç <hasan_tugra1999@outlook.com>
2021-02-21 13:56:39 +0300

    ClassAndMethod

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi");
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi");
+        }
+    }
+}
+
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Hasan";
+            musteri1.Soyad = "Aykaç";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Mehmet";
+            musteri2.Soyad = "Buğra";
+
+            Musteri[] musteris = new Musteri[] { musteri1, musteri2 };
+
+
+            foreach (var m in musteris)
+            {
+                Console.WriteLine("ID: " + m.Id + " " + "Ad: " + " " + m.Ad + " " + "Soyad: " + m.Soyad);
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Listele(musteri1);
+            musteriManager.Ekle(musteri1);
+            musteriManager.Silme(musteri1);
+
+
+        }
+    }
+}
+
